ca9104577ea3bd60d893637ad965a70de9f63a54
JoshHilliker <44536738+JoshHilliker@users.noreply.github.com>
2019-01-08 13:37:05 -0800

    Update yumstart.sh

--- yumstart.sh
+++ yumstart.sh
@@ -1,32 +1,36 @@
-#!/bin/bash             
-#reference, not for production. .  quick & simple way to get all pre req's for what we need.   
-yum install  epel-release -y
-yum install  ca-certificates -y
-yum install  update -y
-yum install  @Development tools -y
-yum install  psmisc    -y                                                                                                                                            
-yum install  util-linux     -y                                                                                                                                          
-yum install  coreutils      -y                                                                                                                                          
-yum install  xfsprogs      -y                                                                                                                                           
-yum install  e2fsprogs     -y                                                                                                                                           
-yum install  findutils     -y                                                                                                                                           
-yum install  git      -y                                                                                                                                                
-yum install  wget      -y                                                                                                                                               
-yum install  bzip2      -y                                                                                                                                              
-yum install  kernel-devel       -y                                                                                                                                      
-yum install  perf      -y                                                                                                                                               
-yum install  blktrace       -y                                                                                                                                          
-yum install  lsof       -y                                                                                                                                              
-yum install  redhat-lsb    -y                                                                                                                                           
-yum install  sysstat        -y                                                                                                                                          
-yum install  python-yaml      -y                                                                                                                                        
-yum install  ipmitool      -y                                                                                                                                           
-yum install  dstat        -y                                                                                                                                            
-yum install  zlib-devel      -y                                                                                                                                         
-yum install  ntp           -y                                                                                                                                           
-yum install  collectl       -y                                                                                                                                          
-yum install  tree         -y                                                                                                                                            
-yum install  screen      -y                                                                                                                                             
-yum install  nvme-cli     -y                                                                                                                                            
-yum install  iperf       -y                                                                                                                                             
+#!/usr/bin/sh
+yum update
+yum install epel-release
+yum install ca-certificates
+
+
+
+ yum install  ca-certificates -y
+ yum install  update -y
+ yum install  @Development tools -y
+ yum install  psmisc    -y                                                                                                                                            
+ yum install  util-linux     -y                                                                                                                                          
+ yum install  coreutils      -y                                                                                                                                          
+ yum install  xfsprogs      -y                                                                                                                                           
+ yum install  e2fsprogs     -y                                                                                                                                           
+ yum install  findutils     -y                                                                                                                                           
+ yum install  git      -y                                                                                                                                                
+ yum install  wget      -y                                                                                                                                               
+ yum install  bzip2      -y                                                                                                                                              
+ yum install  kernel-devel       -y                                                                                                                                      
+ yum install  perf      -y                                                                                                                                               
+ yum install  blktrace       -y                                                                                                                                          
+ yum install  lsof       -y                                                                                                                                              
+ yum install  redhat-lsb    -y                                                                                                                                           
+ yum install  sysstat        -y                                                                                                                                          
+ yum install  python-yaml      -y                                                                                                                                        
+ yum install  ipmitool      -y                                                                                                                                           
+ yum install  dstat        -y                                                                                                                                            
+ yum install  zlib-devel      -y                                                                                                                                         
+ yum install  ntp           -y                                                                                                                                           
+ yum install  collectl       -y                                                                                                                                          
+ yum install  tree         -y                                                                                                                                            
+ yum install  screen      -y                                                                                                                                             
+ yum install  nvme-cli     -y                                                                                                                                            
+ yum install  iperf       -y                                                                                                                                             
  
