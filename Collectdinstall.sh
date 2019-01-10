4d98b4ed74546fd99646407cac94cdf84eda7e50
JoshHilliker <44536738+JoshHilliker@users.noreply.github.com>
2019-01-10 12:51:34 -0800

    Update Collectdinstall.sh

--- Collectdinstall.sh
+++ Collectdinstall.sh
@@ -2,10 +2,10 @@
 # setup for collectd initially.
 # REFERENCE - not for production
 yum install epel-release.noarch
-yum install collectd collectd-rrdtool 
+yum install collectd collectd-rrdtool
 # NOTE:  need to create github to use for both ansible & then put collectd.conf 
 cd /etc
 wget https://raw.githubusercontent.com/JoshHilliker/Telemetry-Infra/master/collectd.conf
 #Grab collectd.conf file from the github
 systemctl enable collectd
-systemctl start collectd 
+systemctl start collectd
