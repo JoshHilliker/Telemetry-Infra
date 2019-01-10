df28938ed23a47a6de05aff0b9f230af3d3c5dcd
JoshHilliker <44536738+JoshHilliker@users.noreply.github.com>
2019-01-10 12:41:56 -0800

    updates

--- grafana.sh
+++ grafana.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/sh
-wget https://dl.grafana.com/oss/release/grafana-5.4.2-1.x86_64.rpm 
-sudo yum localinstall grafana-5.4.2-1.x86_64.rpm 
+#!/bin/bash
+wget https://dl.grafana.com/oss/release/grafana-5.4.2-1.x86_64.rpm
+sudo yum localinstall grafana-5.4.2-1.x86_64.rpm
 sudo systemctl daemon-reload
 sudo systemctl start grafana-server
 sudo systemctl enable grafana-server.service
-	
+
