f7857b03b9263719a3257208110882aca746af1b
JoshHilliker <44536738+JoshHilliker@users.noreply.github.com>
2019-05-15 08:46:54 -0700

    Update Prometheusinstall.sh
    updated release version, however it would be better to go find the new version & automatically place that in the export RELEASE variable.

--- Prometheusinstall.sh
+++ Prometheusinstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Installation of Prometheus  - REFERENCE ONLY - not for production 
+# Installation of Prometheus  - REFERENCE ONLY - not for production
 
 sudo groupadd --system prometheus
 sudo useradd -s /sbin/nologin --system -g prometheus prometheus
@@ -7,9 +7,8 @@ sudo mkdir /var/lib/prometheus
 for i in rules rules.d files_sd; do sudo mkdir -p /etc/prometheus/${i}; done
 sudo yum -y install wget
 cd /tmp
-export RELEASE=2.7.0
-wget https://github.com/prometheus/prometheus/releases/download/v${RELEASE}/
-prometheus-${RELEASE}.linux-amd64.tar.gz
+export RELEASE=2.9.2
+wget https://github.com/prometheus/prometheus/releases/download/v2.9.2/prometheus-2.9.2.linux-amd64.tar.gz
 tar xvf prometheus-${RELEASE}.linux-amd64.tar.gz
 cd prometheus-${RELEASE}.linux-amd64/
 sudo cp prometheus promtool /usr/local/bin/
