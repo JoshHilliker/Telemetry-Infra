60a3df56a3fb4a638797e7df9706d495553fa400
JoshHilliker <44536738+JoshHilliker@users.noreply.github.com>
2019-07-31 09:23:26 -0700

    Update Collectdinstall.sh
    shifted it to sections based on use cases, added notes & references to intel_rdt & intel_pmu.

--- Collectdinstall.sh
+++ Collectdinstall.sh
@@ -4,12 +4,34 @@
 yum install epel-release.noarch
 yum install collectd
 yum install collectd-rrdtool
-yum install collectd-ipmi
-yum install mcelog
 yum install numactl
-# NOTE:  need to create github to use for both ansible & then put collectd.conf 
-cd /etc
-wget -N https://raw.githubusercontent.com/JoshHilliker/Telemetry-Infra/master/collectd.conf
-#Grab collectd.conf file from the github
+
+#IPMI Metrics - recommended no mods to config block
+yum install collectd-ipmi
+yum install ipmitool
+
+#memory
+yum install mcelog
+yum install collectd-mcelog
+modprobe mce-inject
+systemctl start mcelog
+
+#storage
+yum install smartmontools
+yum install collectd-smartmontools
+#check the config block in collectd.conf for the right disks to get smart data on
+
+#Intel_RDT - recommend to get plugin from Barometer or pull down latest tarball from collectd.org - check collectd.conf on github for config block
+#Intel_PMU - recommend to get intel_pmu from https://github.com/andikleen/pmu-tools, then talk to your Intel rep to get Intel_PMU plugin & config block info
+
+#recommend to check the collectd.conf file on https://raw.githubusercontent.com/JoshHilliker/Telemetry-Infra/master/collectd.conf, however each use case needs slight modifications and dependent on your infrastructure.    I have posted two different versions for reference.  Please reach out if any questions.   
+
+
+
 systemctl enable collectd
 systemctl start collectd
+
+
+
+
+
