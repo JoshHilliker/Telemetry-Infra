cb1f7c9bcbad0b60b81d41cc142e6516c00c18d9
JoshHilliker <44536738+JoshHilliker@users.noreply.github.com>
2020-07-28 14:07:09 -0700

    removed legacy
    removing legacy files.

--- legacy/yumstart.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-yum update -y
-
-yum install epel-release ca-certificates @Development tools psmisc util-linux coreutils xfsprogs e2fsprogs findutils git wget bzip2 kernel-devel perf blktrace lsof redhat-lsb sysstat python-yaml ipmitool dstat zlib=devel ntp collectl tree screen nvme-cli -y
-
-echo "ready to go"
