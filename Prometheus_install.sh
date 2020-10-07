5799f6395f846b1ab635b90879d2681b0be9292d
JoshHilliker <44536738+JoshHilliker@users.noreply.github.com>
2020-10-07 07:24:37 -0700

    updated versions
    Updated version of packages to latest & utilized a different method for creating prometheus.yml & prometheus.service so there is no dependency on github to pull those files.

--- Prometheus_install.sh
+++ Prometheus_install.sh
@@ -7,20 +7,64 @@ sudo mkdir /var/lib/prometheus
 for i in rules rules.d files_sd; do sudo mkdir -p /etc/prometheus/${i}; done
 sudo yum -y install wget
 cd /tmp
-export RELEASE=2.9.2
-wget https://github.com/prometheus/prometheus/releases/download/v2.9.2/prometheus-2.9.2.linux-amd64.tar.gz
+export RELEASE=2.21.0
+wget https://github.com/prometheus/prometheus/releases/download/v2.9.2/prometheus-${RELEASE}.linux-amd64.tar.gz
 tar xvf prometheus-${RELEASE}.linux-amd64.tar.gz
 cd prometheus-${RELEASE}.linux-amd64/
 sudo cp prometheus promtool /usr/local/bin/
 sudo cp -r consoles/ console_libraries/ /etc/prometheus/
-cd /etc/prometheus/
-wget https://raw.githubusercontent.com/JoshHilliker/Telemetry-Infra/master/prometheus.yml
-cd /etc/systemd/system/
-wget https://raw.githubusercontent.com/JoshHilliker/Telemetry-Infra/master/prometheus.service
+
+echo "
+#Reference Example - not for Production 
+# Global config
+global: 
+  scrape_interval:     15s # Set the scrape interval to every 15 seconds. Default is every 1 minute.  
+  evaluation_interval: 15s # Evaluate rules every 15 seconds. The default is every 1 minute.  
+  scrape_timeout: 15s  # scrape_timeout is set to the global default (10s).
+
+# A scrape configuration containing exactly one endpoint to scrape:# Here it's Prometheus itself.
+scrape_configs:
+  # The job name is added as a label `job=<job_name>` to any timeseries scraped from this config.
+  - job_name: 'prometheus'
+
+    # metrics_path defaults to '/metrics'
+    # scheme defaults to 'http'.
+
+    static_configs:
+    - targets: ['localhost:9090']" > /etc/prometheus/prometheus.yml
+
+echo "
+[Unit]
+Description=Prometheus
+Documentation=https://prometheus.io/docs/introduction/overview/
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+Type=simple
+Environment="GOMAXPROCS=2"
+User=prometheus
+Group=prometheus
+ExecReload=/bin/kill -HUP $MAINPID
+ExecStart=/usr/local/bin/prometheus \
+  --config.file=/etc/prometheus/prometheus.yml \
+  --storage.tsdb.path=/var/lib/prometheus \
+  --web.console.templates=/etc/prometheus/consoles \
+  --web.console.libraries=/etc/prometheus/console_libraries \
+  --web.listen-address=0.0.0.0:9090 \
+  --web.external-url=
+
+SyslogIdentifier=prometheus
+Restart=always
+
+[Install]
+WantedBy=multi-user.target" > /etc/systemd/system/prometheus.service
+
 rm -rf prometheus-${RELEASE}.linux-amd64.tar.gz
 rm -rf prometheus-${RELEASE}.linux-amd64/
 for i in rules rules.d files_sd; do sudo chown -R prometheus:prometheus /etc/prometheus/${i}; done
 for i in rules rules.d files_sd; do sudo chmod -R 775 /etc/prometheus/${i}; done
 sudo chown -R prometheus:prometheus /var/lib/prometheus/
 sudo systemctl daemon-reload
-systemctl
+systemctl start prometheus
+systemctl enable prometheus
--- grafana.sh
+++ grafana.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
-wget https://dl.grafana.com/oss/release/grafana-5.4.2-1.x86_64.rpm
-sudo yum localinstall grafana-5.4.2-1.x86_64.rpm
+wget https://dl.grafana.com/oss/release/grafana-7.2.0-1.x86_64.rpm
+sudo yum install grafana-7.2.0-1.x86_64.rpm
+#wget https://dl.grafana.com/oss/release/grafana-5.4.2-1.x86_64.rpm
+#sudo yum localinstall grafana-5.4.2-1.x86_64.rpm
 sudo systemctl daemon-reload
 sudo systemctl start grafana-server
 sudo systemctl enable grafana-server.service
