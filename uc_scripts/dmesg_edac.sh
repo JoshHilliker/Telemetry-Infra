411d1193300228223441ac8c243b2e65cdf4d1b7
JoshHilliker <44536738+JoshHilliker@users.noreply.github.com>
2021-01-17 12:31:47 -0800

    Update dmesg_edac.sh

--- uc_scripts/dmesg_edac.sh
+++ uc_scripts/dmesg_edac.sh
@@ -1,5 +1,7 @@
+#! /bin/bash
+# purpose to do daily pull of DMESG in order to grab EDAC data (Error Detection and Correction driver) to utilize with a machine learning model for memory failure prediction. 
 
 while /bin/true ; do
    dmesg | grep EDAC >> $(hostname)_test.txt
-  sleep 30
+  sleep 8640
 done
